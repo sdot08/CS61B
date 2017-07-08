48770f638775f2f71439ebd20a5d41bb20063abf
Byron Sun <ys3202@nyu.edu>
2017-07-08 15:54:49 +0800

    07.08

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -8,13 +8,15 @@ public class ArrayDeque <Item>{
 
     Item[] items;
     int size;
-    int front=0;
-    int rear=1;
+    int front;
+    int rear;
 
     /** Creates an empty list. */
     public ArrayDeque() {
         size=0;
         items=(Item[]) new Object[8];
+        front=-1;
+        rear=0;
     }
     public void resize(int capacity){
 
@@ -22,6 +24,10 @@ public class ArrayDeque <Item>{
     }
     /** Adds an item to the front of the Deque.*/
     public void addFirst(Item item){
+        if(front==-1 && rear==0){
+            front=0;
+            rear=1;
+        }
         if(size==items.length)
             resize(size*2);
         if(front==-1)
@@ -29,11 +35,14 @@ public class ArrayDeque <Item>{
         items[front]=item;
         front--;
         size++;
+        if(front==-1)
+            front=items.length-1;
     }
 
 
     /**  Adds an item to the back of the Deque. */
     public void addLast(Item item){
+
         if(size==items.length)
             resize(size*2);
         if(rear==items.length)
@@ -41,6 +50,9 @@ public class ArrayDeque <Item>{
         items[rear]=item;
         rear++;
         size++;
+
+        if(rear==items.length)
+            rear=0;
 
     }
 
@@ -52,6 +64,32 @@ public class ArrayDeque <Item>{
             return false;
     }
 
+    /** Removes and returns the item at the front of the Deque. If no such item exists, returns null. */
+    public Item removeFirst(){
+        if(isEmpty())
+            return null;
+        int index;
+        if(front==7)
+            front=-1;
+        Item item=items[front+1];
+        items[front+1]=null;
+        front++;
+        size--;
+        return item;
+    }
+
+    public Item removeLast(){
+        if(isEmpty())
+            return null;
+        if(rear==0)
+            rear=items.length;
+        Item item=items[rear-1];
+        items[rear-1]=null;
+        rear--;
+        size--;
+        return item;
+    }
+
 
     /** Returns the number of items in the Deque. */
     public int size() {
@@ -61,15 +99,21 @@ public class ArrayDeque <Item>{
     /** Prints the items in the Deque from first to last, separated by a space. */
     public void printDeque(){
         int i=0;
-        for(i=front+1;i<items.length && i!=rear+1;i++)
-            System.out.println(items[i]);
+        for(i=front+1;i<items.length && i!=rear;i++)
+            System.out.print(items[i]+" ");
         if(i-1==rear)
             return;
         if(i==items.length){
             for(i=0;i<rear;i++)
-                System.out.println(items[i]);
+                System.out.print(items[i]+" ");
         }
 
+    }
+
+    public Item get(int index){
+        if(isEmpty())
+            return null;
+        return items[index];
     }
 
 
--- proj1/ArrayDequeTest.java
+++ proj1/ArrayDequeTest.java
@@ -5,19 +5,89 @@ import org.junit.Test;
 public class ArrayDequeTest {
 
 
-    @Test
-    public void testprint() {
-        ArrayDeque<Integer> a = new ArrayDeque<>();
-        a.addFirst(3);
-        a.addLast(5);
-        a.addFirst(4);
-        a.printDeque();
-    }
+
 
     @Test
     public void testisEmpty() {
         ArrayDeque<Integer> a = new ArrayDeque<>();
         assertTrue(a.isEmpty());
+    }
+
+    @Test
+    public void testremoveFirst() {
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        a.addLast(3);
+        a.addLast(5);
+        a.addFirst(4);
+        a.removeFirst();
+        a.removeFirst();
+        a.addLast(10);
+        a.addFirst(1);
+
+        ArrayDeque<Integer> b = new ArrayDeque<>();
+        b.addLast(1);
+        b.addLast(5);
+        b.addLast(10);
+        assertEquals(a.items,b.items);
+
+        a.removeFirst();
+        a.removeFirst();
+        a.addLast(1);
+        a.addLast(2);
+        a.addLast(3);
+        a.addLast(4);
+        a.addLast(5);
+        a.addLast(6);
+        /* a.printDeque(); */
+
+
+    }
+
+    @Test
+    public void testremoveLast() {
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        a.addLast(3);
+        a.addLast(5);
+        a.removeFirst();
+        a.removeFirst();
+        a.addLast(10);
+        a.addFirst(1);
+        a.addLast(1);
+        a.addLast(2);
+        a.addLast(3);
+        a.addLast(4);
+        a.addLast(5);
+        /* a.printDeque(); */
+        a.addLast(6);
+
+        ArrayDeque<Integer> b = new ArrayDeque<>();
+        b.addLast(6);
+        b.addLast(1);
+        b.addLast(10);
+        b.addLast(1);
+        b.addLast(2);
+        b.addLast(3);
+        b.addLast(4);
+        b.addLast(5);
+
+        assertEquals(a.items,b.items);
+
+        a.removeLast();
+        a.removeLast();
+        b.removeFirst();
+        b.removeLast();
+        assertEquals(a.items,b.items);
+    }
+
+    @Test
+    public void testget() {
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        a.addLast(3);
+        a.addLast(5);
+        int one=a.get(0);
+
+        assertEquals(one, 3);
+        assertEquals(a.get(5), null);
     }
 
 
